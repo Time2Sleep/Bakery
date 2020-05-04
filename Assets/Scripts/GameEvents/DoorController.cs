b83a042687bb1c0f0c56dfd7005752eb10dd62d0
Airat Zaripov <omg.airat@hotmail.com>
2020-05-04 18:32:15 +0300

    Events example

---
+++ Assets/Scripts/GameEvents/DoorController.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DoorController : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        GameEvents.current.onDoorwayTriggerEnter += OnDoorwayOpen;
+    }
+
+    private void OnDoorwayOpen()
+    {
+        transform.position += transform.up * 2;
+        GameEvents.current.onDoorwayTriggerEnter -= OnDoorwayOpen;
+        GameEvents.current.onDoorwayTriggerEnter += OnDoorwayClose;
+    }
+    
+    private void OnDoorwayClose()
+    {
+        transform.position -= transform.up * 2;
+        GameEvents.current.onDoorwayTriggerEnter += OnDoorwayOpen;
+        GameEvents.current.onDoorwayTriggerEnter -= OnDoorwayClose;
+    }
+}
